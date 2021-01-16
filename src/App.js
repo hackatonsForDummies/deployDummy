c7cae5db49299645c5196e8eb4e12f343392c672
pabloPc <pjorda96@gmail.com>
2021-01-16 12:08:58 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/hackatonsForDummies"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit us
         </a>
       </header>
     </div>
